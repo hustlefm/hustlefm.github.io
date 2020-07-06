82b83dc62e0c7cb2c768ea2aca53b3310e034333
k.tribunsky <k.tribunsky@bi.zone>
2020-07-06 19:05:31 +0300

    update

--- dist/js/ices.js
+++
@@ -1,1 +0,0 @@
-!function(n){function t(t){var e=t.host,r=t.port||80;return{refresh:function(t,o){setInterval(function(){n.ajax({url:"http://"+e+":"+r+"/stream_info.php",success:function(n){o(JSON.parse(n))}})},t)}}}window.Ices||(window.Ices=t)}(jQuery);
